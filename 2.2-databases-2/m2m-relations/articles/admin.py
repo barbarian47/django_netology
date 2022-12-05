6dbedc418ed737724a7c17932dfafda03b305033
Alexey Serebryakov <barbarians47@outlook.com>
2022-12-06 02:38:13 +0300

    v1.0

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main = None
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                if main is not None:
+                    raise ValidationError('Основной раздел должен быть один')
+                main = form.cleaned_data['is_main']
+        if main is None:
+            raise ValidationError('Нет основного раздела')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    admin_list = ['id', 'name']
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    admin_list = ['id', 'title', 'published_at']
+    filter_list = ['published_at']
+    inlines = [ScopeInline]
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,10 +5,11 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    ordering = Article.objects.order_by('-published_at')
+    context = {'ordering': ordering}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    # ordering = '-published_at'
 
     return render(request, template, context)
