364d1cf0a0938a6387401a4d256ae324fcd3bcdf
Alexey Serebryakov <barbarians47@outlook.com>
2022-12-16 22:11:20 +0300

    3.4_fix_valid_test_path

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -7,17 +7,9 @@ from .models import Course, Student
 class CourseSerializer(serializers.ModelSerializer):
 
 
-
     class Meta:
         model = Course
         fields = ("id", "name", "students")
-
-    # def validate(self, attrs):
-    #     count = Student.objects.count()
-    #     if count > settings.MAX_STUDENTS_PER_COURSE:
-    #         raise serializers.ValidationError('Limit 20 students per course')
-    #
-    #     return super().validate(attrs)
 
     def validate_students(self, value):
         if len(Student.objects.all()) > settings.MAX_STUDENTS_PER_COURSE:
